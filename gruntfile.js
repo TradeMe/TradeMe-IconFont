5f1a513adda4036df3bfff47b4635141ca2c1e95
Jacob Lapworth <jacob.lapworth@trademe.co.nz>
2017-07-13 14:30:36 +1200

    Compress build files for release

--- gruntfile.js
+++ gruntfile.js
@@ -1,55 +1,81 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        webfont: {
-            // iconfont build and styles for production
-            production: {
-                src: 'src/icons/*.svg',
-                dest: 'iconfont/production',
-                options: {
-                    template: 'src/templates/icons.scss',
-                    templateOptions: {
-                      baseClass: 'tmicon',
-                      classPrefix: 'tmicon-'
-                    },
-                    customOutputs: [{
-                      template: 'src/templates/codepoints',
-                      dest: 'iconfont'
-                    }],
-                    stylesheets: ['css','scss'],
-                    hashes: true,
-                    font: 'tmicons',
-                    styles: 'font,icon',
-                    types: 'eot,woff,ttf,svg',
-                    order: 'eot,woff,ttf,svg',
-                    fontHeight: 960,
-                    descent: 0,
-                    codepointsFile: 'src/codepoints',
-                    htmlDemo: true,
-                    htmlDemoTemplate: 'src/templates/tmicons-demo.html',
-                    destHtml: 'iconfont'
-                }
-            },
-            // iconfont for Sketch App toolkit
-            sketchtoolkit: {
-                src: 'src/icons/*.svg',
-                dest: 'iconfont/sketch-toolkit',
-                options: {
-                    font: 'tmicons',
-                    types: 'ttf',
-                    fontHeight: 960,
-                    descent: 0,
-                    ligatures: true,
-                    stylesheets: [],
-                    htmlDemo: false
-                }
-            }
+  grunt.initConfig({
+    webfont: {
+      // iconfont build and styles for production
+      production: {
+        src: 'src/icons/*.svg',
+        dest: 'iconfont/production',
+        options: {
+          template: 'src/templates/icons.scss',
+          templateOptions: {
+            baseClass: 'tmicon',
+            classPrefix: 'tmicon-'
+          },
+          customOutputs: [{
+            template: 'src/templates/codepoints',
+            dest: 'iconfont'
+          }],
+          stylesheets: ['css', 'scss'],
+          hashes: true,
+          font: 'tmicons',
+          styles: 'font,icon',
+          types: 'eot,woff,ttf,svg',
+          order: 'eot,woff,ttf,svg',
+          fontHeight: 960,
+          descent: 0,
+          codepointsFile: 'src/codepoints',
+          htmlDemo: true,
+          htmlDemoTemplate: 'src/templates/tmicons-demo.html',
+          destHtml: 'iconfont'
         }
-    });
+      },
+      // iconfont for Sketch App toolkit
+      sketchtoolkit: {
+        src: 'src/icons/*.svg',
+        dest: 'iconfont/sketch-toolkit',
+        options: {
+          font: 'tmicons',
+          types: 'ttf',
+          fontHeight: 960,
+          descent: 0,
+          ligatures: true,
+          stylesheets: [],
+          htmlDemo: false
+        }
+      }
+    },
+    // package build files for release
+    compress: {
+      production: {
+        options: {
+          archive: 'dist/tmicons-production.zip'
+        },
+        files: [{
+          expand: true,
+          cwd: 'iconfont/production/',
+          src: ['**/*'],
+          dest: ''
+        }]
+      },
+      sketchtoolkit: {
+        options: {
+          archive: 'dist/tmicons-sketch.zip'
+        },
+        files: [{
+          expand: true,
+          cwd: 'iconfont/sketch-toolkit/',
+          src: ['**/*'],
+          dest: ''
+        }]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-webfont');
+  grunt.loadNpmTasks('grunt-webfont');
+  grunt.loadNpmTasks('grunt-contrib-compress');
 
-    // Default task(s).
-    grunt.registerTask('default', ['webfont']);
+  // Default task(s).
+  grunt.registerTask('default', ['webfont', 'compress']);
 
 };
